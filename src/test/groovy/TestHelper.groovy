34555ecc6d12f981f3c0a3f7e94df9f4534f69a9
stefunk <stefanos.kirikakis@nokia.com>
2017-11-15 00:28:29 +0200

    Adding wrapper jar

--- src/test/groovy/TestHelper.groovy
+++ src/test/groovy/TestHelper.groovy
@@ -1,8 +1,5 @@
 package com.github.kirikakis.marvel.character
 
-import java.nio.file.Path
-import java.nio.file.Paths
-
 class TestHelper {
     static def ReadFileFromResources(String fileName) {
         return new File('src/test/resources/' + fileName).text
